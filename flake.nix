2554dbae0deed6ba9b79d623a89a2d52162246af
Gary Glover <dev@thebigg69.co.uk>
2024-01-18 18:32:28 +0000

    All flake based builds

--- flake.nix
+++ flake.nix
@@ -1,9 +1,10 @@
 {
-  description = "My Nix Package Repository";
+  description = "GarySGlover Nix Package Repository";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
     ttop.url = "path:./ttop";
+    futhark.url = "path:./futhark";
   };
 
   outputs = {
@@ -11,11 +12,13 @@
     flake-utils,
     nixpkgs,
     ttop,
+    futhark,
     ...
   } @ inputs: (flake-utils.lib.eachDefaultSystem (system: let
   in rec {
     packages = {
       ttop = ttop.outputs.packages.${system}.ttop;
+      futhark = futhark.outputs.packages.${system}.futhark;
     };
   }));
 }
